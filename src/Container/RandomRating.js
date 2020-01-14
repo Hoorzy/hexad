9f5c0d9188aba4b02fd34d991b38b40eccba2410
hoorinaz <nazari.hoorie@yahoo.com>
2020-01-14 17:35:37 +0100

    create table and buttons

---
+++ src/Container/RandomRating.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+export default class RandomRating extends React.Component{ 
+    constructor(props){
+        super(props)
+      }
+    render (){
+        return (
+            <div>
+                <Button variant="contained" color="primary">Random Rating</Button>
+                <Button variant="contained" color="secondary">Stop Rating</Button>
+
+            </div>
+
+        )
+    }
+
+}
