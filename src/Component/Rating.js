70990dc6cdc125597cf5123b9b00087531c84469
Hoorzy <nazari.hoorie@gmail.com>
2020-01-20 10:27:53 +0100

    debugg

--- src/Component/Rating.js
+++ src/Component/Rating.js
@@ -1,144 +1,41 @@
+//editable table
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-
-import EditIcon from '@material-ui/icons/Edit';
-import CheckIcon from '@material-ui/icons/Check';
-
-import Movies from '../Movies.json'
-import { checkServerIdentity } from 'tls';
-import TextField from '@material-ui/core/TextField';
+import MaterialTable from 'material-table';
 
 
-const columns = [
-  { id: 'name', label: 'Movie', minWidth: 170 },
-  { id: 'rating', label: 'Rating', minWidth: 100 },
-  {
-    id: 'edit',
-    label: 'Edit',
-    minWidth: 50,
-    align: 'center',
-    // format: value => value.toLocaleString(),
-  },
 
-];
-var currentlyEditing = true
-var i = 3 
-
-function createData(name, rating , edit) {
-    return { name, rating , edit};
-  }
-  var arr=[]
-  Object.keys(Movies.data).map(key => {  
-    arr.push(createData(Movies.data[key].movie , Movies.data[key].rating ))
-  })
-
-
-const rows = arr ; 
-
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  container: {
-    maxHeight: 440,
-  },
-});
-
-export default function StickyHeadTable() {
-  const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+export default function MaterialTableDemo(props) {
+ 
+  const [state, setState] = React.useState({
+    columns: [
+       { title: 'Movie', field: 'movie' },
+      { title: 'Rating', field: 'rating' },
+    ],
+    data: props.rows
+  });
 
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-
-
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row , index) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map(column => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-
-                        {column.id === 'edit' && !currentlyEditing ?
-                         <EditIcon onClick={()=>{
-
-                            //  startEditing()
-                             alert("Hoorie" + index  ) 
-
-                            }} />
-                         : column.id === 'edit' && currentlyEditing && index === i ? 
-                         <CheckIcon onClick={()=>{
-
-                            //  stopEditing()
-                             alert("Hoorie" + index  ) 
-
-                            }} />
-
-                        : column.id === 'rating' && currentlyEditing && index === i? 
-                         <TextField 
-                         name = {column.id}
-                        //  onChange ={handelChange}
-                         value = {value}
-                        
-                          />
-                            :value}
-                        {/* {value} */}
-                      </TableCell>
-                    //   <TableCell> <EditIcon/> </TableCell>
-                    );
-                  })}
-
-                  
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <div>
+    <MaterialTable
+      title="Movies"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+      }}
+    />
+    </div>
   );
 }
--- src/Container/RandomRating.js
+++ src/Container/RandomRating.js
@@ -1,77 +1,84 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-
 import Movies from '../Movies.json';
 import Rating from '../Component/Rating';
 
-export default class RandomRating extends React.Component{ 
-    constructor(props){
-        super(props)
-        let rows=[]
-        Object.keys(Movies.data).map(key => {  
-            rows.push({ movie: Movies.data[key].movie , rating : Movies.data[key].rating })
-        })
-        this.state = {status : false , data : rows };
-    }
-    checkStatus = () => {
-      this.setState({
-        status: !this.state.status
-      });
-      
-    }
-    // ================================================================
-    componentDidUpdate() {
-      if (this.state.status) {
-        this.randomRating(true);
-      } else {
-        this.randomRating(false);
-      }
-    }
-    // ================================================================
-    randomRating = (go) => {
-      if (go) {
-        let newData = [... this.state.data]
-        let randomTime = this.getRandomInt(7);
-        if (randomTime <= 0) {
-          randomTime = 1;
-        }
-        randomTime = randomTime * 1000;
-  
-        console.log(`Started ${randomTime}`);
-        this.time = setInterval(() => this.stuff(newData) , randomTime);
-        } 
-      if(!go) {
-          clearInterval(this.time);
-          console.log("Stoped!");
-    
-      }
-    }
-
-    stuff = (newData) =>{
-      let randomRate = (Math.random() * (10)).toFixed(1);
-      let randomItem = this.getRandomInt(this.state.data.length);
-      newData[randomItem].rating = randomRate
-      console.log(newData);
-      newData.sort((a, b) => (a.rating > b.rating) ? 1 : -1)
-      this.setState({
-        data : newData
+export default class RandomRating extends React.Component {
+  constructor(props) {
+    super(props)
+    let rows = []
+    Object.keys(Movies.data).map(key => {
+      rows.push({ movie: Movies.data[key].movie, rating: Movies.data[key].rating })
     })
-    }
+    this.state = { randomlyRatingStatus: false, data: rows };
+  }
+  checkStatus = () => {
+    this.setState({
+      randomlyRatingStatus: !this.state.randomlyRatingStatus
+    });
 
-       getRandomInt = max => {
-           let number = Math.floor(Math.random() *(max))
-        return number;
+  }
+  // ================================================================
+  componentDidUpdate() {
+    if (this.state.randomlyRatingStatus) {
+      this.randomRating(true);
+    } else {
+      this.randomRating(false);
+    }
+  }
+  // ================================================================
+  randomRating = (go) => {
+    if (go) {
+      let newData = [... this.state.data]
+      let randomTime = this.getRandomInt(7);
+      if (randomTime <= 0) {
+        randomTime = 1;
       }
+      randomTime = randomTime * 1000;
 
-    render (){
-        return (
-            <div>
-                <Rating rows = {this.state.data.sort((a, b) => (a.rating < b.rating) ? 1 : -1)}/>
-                <Button variant="contained" color="primary" onClick={this.checkStatus}>Random Rating</Button>
+      console.log(`Started ${randomTime}`);
 
-            </div>
+      this.setState(() => {
+        time : setInterval(() => {
 
-        )
+          this.stuff(newData)
+        }
+          , randomTime);
+      })
+
     }
+    if (!go) {
+      clearInterval(this.state.time);
+      console.log("Stoped!");
+
+    }
+  }
+
+  stuff = (newData) => {
+    let randomRate = (Math.random() * (10)).toFixed(1);
+    let randomItem = this.getRandomInt(this.state.data.length);
+    newData[randomItem].rating = randomRate
+    console.log(newData);
+    newData.sort((a, b) => (a.rating > b.rating) ? 1 : -1)
+    this.setState({
+      data: newData
+    })
+  }
+
+  getRandomInt = max => {
+    let number = Math.floor(Math.random() * (max))
+    return number;
+  }
+
+  render() {
+    return (
+      <div>
+        <Rating rows={this.state.data.sort((a, b) => (a.rating < b.rating) ? 1 : -1)} />
+        <Button variant="contained" color="primary" onClick={this.checkStatus}>Random Rating</Button>
+
+      </div>
+
+    )
+  }
 
 }
